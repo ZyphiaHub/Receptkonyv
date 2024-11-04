1f16a4cd6624499a47c8774f77c34e712b4abf2b
ZyphiaHub <149183538+ZyphiaHub@users.noreply.github.com>
2024-11-04 23:37:39 +0100

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
-import adapter from '@sveltejs/adapter-static';
-
-
+import adapter from '@sveltejs/adapter-auto';
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-		  // optional configuration for GitHub Pages
-		  fallback: '200.html'
-		}),
-		paths: {
-		  base: '/Receptkonyv'
-		}
-	  }
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter()
+	}
 };
-
 export default config;
